f0906a1e4d183344b9486c1bce21da25499a3581
thomas <koffient>
2015-06-18 15:45:59 +0200

    Add default Bootstrap templating

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.default')
+
+@section('content')
+	<div class="container">
+		<h1>Laravel 5 Project Starter</h1>
+		<p class="lead">{{ Inspiring::quote() }}</p>
+	</div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<hr>
+				<small class="text-muted">Alle rechten voorbehouden &copy; {{ date('Y') }}</small>
+			</div>
+		</div>
+	</div>
+</footer>
