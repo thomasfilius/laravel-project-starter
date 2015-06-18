d438d04290c8899ebc453c282d6dae5cb8c5318b
thomas <koffient>
2015-06-18 12:57:05 +0200

    Update Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,22 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.less('app.less');
+var paths = {
+	'jquery': 'vendor/bower_components/jquery',
+	'bootstrap': 'vendor/bower_components/bootstrap-sass-official',
+	'fontawesome': 'vendor/bower_components/fontawesome'
+};
+
+elixir(function (mix) {
+	mix.sass('main.scss');
+
+	// Merge vendor scripts
+	mix.scripts([
+		paths.jquery    + '/dist/jquery.js',
+		paths.bootstrap + '/assets/javascripts/bootstrap.js',
+	], 'public/js/vendor.js', __dirname + '/');
+
+	// Copying fonts
+	mix.copy(paths.bootstrap   + '/assets/fonts/bootstrap/**',  'public/fonts')
+	   .copy(paths.fontawesome + '/fonts/**',                   'public/fonts');
 });
